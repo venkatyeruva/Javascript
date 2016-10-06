455b24cd58cf5408c89318b0d23ae40d8c711d3f
venkatyeruva <venkatyeruva@gmail.com>
2016-10-06 15:26:29 +0530

    Sample

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+ 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.get('/views', function (req, res) {
+  res.send('Htest!');
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
